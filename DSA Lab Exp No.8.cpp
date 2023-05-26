6e54f45d1d645f4af347387da3f5749c77f5a36b
Atharva Athanikar <120152105+AtharvaAthanikar2003@users.noreply.github.com>
2023-05-26 20:10:21 +0530

    Update DSA Lab Exp No.8.cpp

--- DSA Lab Exp No.8.cpp
+++ DSA Lab Exp No.8.cpp
@@ -1,7 +1,7 @@
 /*
 Given sequence k = k1 <k2 < … <kn of n sorted keys, with a search probability pi for each
 key ki . Build the Binary search tree that has the least search cost given the access
-probability for each key
+probability for each key.
 */
 
 #include <iostream>
