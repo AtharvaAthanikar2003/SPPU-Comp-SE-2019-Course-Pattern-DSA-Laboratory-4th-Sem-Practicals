b309e3789d5d3514f22b09b627f490d8c575232f
Atharva Athanikar <120152105+AtharvaAthanikar2003@users.noreply.github.com>
2023-05-26 20:19:10 +0530

    Update DSA Lab Exp No.10.cpp

--- DSA Lab Exp No.10.cpp
+++ DSA Lab Exp No.10.cpp
@@ -3,12 +3,11 @@ To create ADT that implement the "set" concept.
 a. Add (newElement) - Place a value into the set
 b. Remove (element) - Remove the value
 c. Contains (element) - Return true if element is in collection
-d. Size () - Return number of values in collection Iterator () Return an iterator used to loop
-over collection
+d. Size () - Return number of values in collection Iterator () Return an iterator used to loop over collection
 e. Intersection of two sets
 f. Union of two sets
 g. Difference between two sets
-h.Subset.
+h. Subset.
 */
 
 #include <iostream>
